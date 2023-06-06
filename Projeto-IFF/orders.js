2a93988a84d4f5530922a089b898ce8e85895cd3
Von Heldh <125845590+VonHeldh@users.noreply.github.com>
2023-06-06 17:52:25 -0300

    Add files via upload

---
+++ Projeto-IFF/orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Português',
+        productNumber: 'Fábio R. Penna',
+        paymentStatus: 'fabio.penna@iff.edu.br',
+        shipping: 'Pendência(s)'
+    },
+    {
+    productName: 'Matemática',
+    productNumber: 'Vandré S. Gomes',
+    paymentStatus: 'vandre.gomes@iff.edu.br',
+    shipping: 'Pendência(s)'
+    },
+    {
+    productName: 'Geografia',
+    productNumber: 'José Felippe S. Peres',
+    paymentStatus: 'jose.peres@iff.edu.br',
+    shipping: 'Expirado'
+    },
+    {
+    productName: 'Filosofia',
+    productNumber: 'Julianna G. Henrique',
+    paymentStatus: 'julianna.henrique@iff.edu.br',
+    shipping: 'Concluído'
+    },
+    {
+    productName: 'História',
+    productNumber: 'Diego G. Porto',
+    paymentStatus: 'diego.porto@iff.edu.br',
+    shipping: 'Concluído'
+    },
+    {
+    productName: 'Química',
+    productNumber: 'Simone A. Silva',
+    paymentStatus: 'simone.silva@iff.edu.br',
+    shipping: 'Concluído'
+    },
+    {
+    productName: 'Biologia',
+    productNumber: 'Márcia R. de Souza',
+    paymentStatus: 'marcia.souza@iff.edu.br',
+    shipping: 'Concluído'
+    },
+]
